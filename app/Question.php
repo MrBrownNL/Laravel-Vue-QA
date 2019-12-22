e849a4ccbcf11860cbe8243b4b3d329b28091209
Jeroen de Bruijn <jeroen@alphega.nl>
2019-12-22 11:41:55 +0100

    Add counters and custom css

--- app/Question.php
+++ app/Question.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $fillable = ['title', 'body'];
-    
+
     public function user() {
         return $this->belongsTo(User::class);
-    }    
+    }
 
     public function setTitleAttribute($value)
     {
@@ -18,9 +18,23 @@ class Question extends Model
         $this->attributes['slug'] = str_slug($value);
     }
 
+    /**
+     * Accessor accessable by using question->url
+     * @return string
+     */
     public function getUrlAttribute()
     {
         return route("questions.show", $this->id);
+    }
+
+    public function getVotesAttribute() {
+        return $this->votes_count;
+    }
+    public function getAnswersAttribute() {
+        return $this->answers_count;
+    }
+    public function getStatusAttribute() {
+        return ($this->besta_answer_id > 0 ? "answer-accepted" : ($this->answers > 0 ? "answered" : "unanswered"));
     }
 
     public function getCreatedDateAttribute()
