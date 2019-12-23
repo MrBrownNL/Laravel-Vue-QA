58e65005edbcb8c7fb4c212ed18648e421cf1c0d
Jeroen de Bruijn <jeroen@alphega.nl>
2019-12-23 11:20:44 +0100

    Auth Policy method

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
+     public function __construct() {
+         $this->middleware('auth')->except('index', 'show');
+     }
+
     /**
      * Display a listing of the resource.
      *
@@ -65,6 +69,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize("update", $question);
 
         return view('questions.edit',compact('question'));
     }
@@ -78,7 +83,10 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize("update", $question);
+
         $question->update($request->only('title', 'body'));
+
         return redirect(route('questions.index'))->with('success',"Your question has been updated");
     }
 
@@ -90,7 +98,10 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize("delete", $question);
+
         $question->delete();
+
         return redirect(route('questions.index'))->with('success','Your question has been deleted');
 
     }
