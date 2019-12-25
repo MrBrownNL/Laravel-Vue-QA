e5cf4901351b7560dcc3906cf4e054108ee57484
Jeroen de Bruijn <jeroen@alphega.nl>
2019-12-25 09:05:43 +0100

    Added purifier to prevent XSS hack

--- app/Question.php
+++ app/Question.php
@@ -21,6 +21,10 @@ class Question extends Model
         $this->attributes['slug'] = str_slug($value);
     }
 
+//    public function setBodyAttribute($value) {
+//        $this->attributes['body'] = clean($value);
+//    }
+
     /**
      * Accessor accessable by using question->url
      * @return string
@@ -40,7 +44,7 @@ class Question extends Model
     }
 
     public function getBodyHtmlAttribute() {
-        return \Parsedown::instance()->text($this->body);
+        return clean($this->bodyHtml());
     }
 
     public function answers() {
@@ -71,4 +75,18 @@ class Question extends Model
         return $this->favorites->count();
     }
 
+    public function getExcerptAttribute()
+    {
+        return $this->excerpt(250);
+    }
+
+    public function excerpt($length)
+    {
+        return str_limit(strip_tags($this->bodyHtml()), $length);
+    }
+
+    private function bodyHtml()
+    {
+        return \Parsedown::instance()->text($this->body);
+    }
 }
